c90dffb94ed1c0006d332d875502d9b7d7be42e2
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-06-05 23:21:32 -0300

    Refactoring Google Stores service and add show request

--- src/services/google_list_of_establishments.js
+++
@@ -1,7 +0,0 @@
-import Api from './api';
-
-const GoogleListOfEstablishmentsService = {
-  index: (latitude, longitude) =>  Api.get(`/google_stores?latitude${latitude}&longitude=${longitude}`)
-}
-
-export default GoogleListOfEstablishmentsService;
