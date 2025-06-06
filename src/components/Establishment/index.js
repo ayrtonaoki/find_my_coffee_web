3849e689d1e95f262c9a81014611e9f8e1d05659
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-06-06 01:40:57 -0300

    Create Establishment index component

--- src/components/Establishment/index.js
+++ src/components/Establishment/index.js
@@ -1,8 +1,35 @@
 import React, {useState, useEffect} from 'react';
 import EstablishmentsService from '../../services/establishments_service';
 
+import styled from 'styled-components';
+
+const LeftBar = styled.div`
+  height: 100%;
+  overflow-y: auto;
+  width: 250px;
+  position: absolute;
+  color: white;
+  background-color: rgba(10,10,10,0.9);
+  padding: 20px;
+`
+
+const Title = styled.h1`
+  font-size: 20px;
+  color: rgba(220, 110, 50, 0.7);
+`
+
+const Paragraph = styled.p`
+  font-size: 13px;
+  line-height: 14px;
+`
+
+const Image = styled.img`
+  height: 150px;
+  width: 100%;
+  `
+
 const Establishment = (props) => {
-  const [Establishment, setEstablishment] = useState([]);
+  const [establishment, setEstablishment] = useState([]);
   const { REACT_APP_GOOGLE_API_KEY } = process.env;
 
   useEffect(() => {
@@ -19,9 +46,33 @@ const Establishment = (props) => {
   }
 
   return (
-    <div>
+    <LeftBar>
+      {
+        (establishment.photos) ?
+          <Image src={`
+            https://maps.googleapis.com/maps/api/place/photo?photoreference=${establishment.photos[0].photo_reference}&key=${REACT_APP_GOOGLE_API_KEY}&maxwidth=400
+          `} alt="Coffee Photo"/>
+        :
+          <Image src="/images/no_photo.jpg" alt="Coffee no photo"/>
+      }
 
-    </div>
+      <Title>{establishment.name}</Title>
+      {
+        (establishment.opening_hours) ?
+          <div> (property) establishment: any[]
+            { (establishment.opening_hours.open_now === true) ? "Aberto" : "Fechado"}
+            <hr/>
+            {
+              establishment.opening_hours.weekday_text.map((schedule, index) => {
+                return(<Paragraph key={index}>{schedule}</Paragraph>)
+              })
+            }
+          </div>
+        : <Paragraph>"Não há cadastro de dias e horário abertos"</Paragraph>
+      }
+      <hr/>
+      <Paragraph>{establishment.formatted_address}</Paragraph>
+    </LeftBar>
   )
 }
 
